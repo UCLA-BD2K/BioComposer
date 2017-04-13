bee9be24419365021769765a096b63604a495a24
nnsookwon <nnsookwon@gmail.com>
2017-04-13 15:00:42 -0700

    Prevent FileWindow and DownloadWindow from being dragged off screen (fixes issue of unwanted scrolling); Window dragHandler extracted to separate windowControllerHelper.js;

--- public/javascripts/downloadWindowController.js
+++ public/javascripts/downloadWindowController.js
@@ -46,39 +46,18 @@ var downloadWindowController = function()
         contentViewParent.append(contentView);
         innerWindow.append(contentViewParent);
         this.div.append(innerWindow);
-        
-        
-        //Add handlers to make draggable
-        $(toolBar).mousedown(function (e) {
-
-            // Disable text highlighting when window is dragged
-            $("body").addClass('disable_text_highlight');
-            $("#fileWindow").addClass('disable_text_highlight');
-            editor.setReadOnly(true);
                 
-            //Grab beginning mouse positions
-            var mouse_pos_x = e.pageX;   
-            var mouse_pos_y = e.pageY;
-            
-            var initPosX = $("#downloadWindow").position().left;
-            var initPosY = $("#downloadWindow").position().top;
+        //Add handler to make draggable
+        $(toolBar).mousedown(function(e){
+            dragHandler(e, "downloadWindow");
+        });
 
-            $(document).mousemove(function (e) {
-                //Grab offset
-                var offset_x = e.pageX - mouse_pos_x;
-                var offset_y = e.pageY - mouse_pos_y;
-                
-                //Readjust CSS
-                $("#downloadWindow").css({"left" : initPosX + offset_x + "px", "top" : initPosY + offset_y + "px"});
-               
-
-            });
-        }).mouseup(function () {
+        $(document).mouseup(function () {
             // Re-enable text highlight
             $("body").removeClass('disable_text_highlight');  
-            $("#fileWindow").removeClass('disable_text_highlight');
+            $("#downloadWindow").removeClass('disable_text_highlight');
             editor.setReadOnly(false);
-            
+
             $(document).unbind('mousemove');
         });
         
---
+++ public/javascripts/windowControllerHelper.js
@@ -0,0 +1,43 @@
+dragHandler = function(e, id) {
+        id = "#" + id;
+            // Disable text highlighting when window is dragged
+            $("body").addClass('disable_text_highlight');
+            $(id).addClass('disable_text_highlight');
+            editor.setReadOnly(true);
+
+            //Window dimenions
+            var window_x = window.innerWidth;
+            var window_y = window.innerHeight;
+
+            //Grab beginning mouse positions
+            var mouse_pos_x = e.clientX;   
+            var mouse_pos_y = e.clientY;
+            
+            var initPosX = $(id).position().left;
+            var initPosY = $(id).position().top;
+
+            //Window position relative to document
+            var initPosX_relative = $(id).offset().left;
+            var initPosY_relative = $(id).offset().top;
+
+            $(document).mousemove(function (e) {
+                //Grab offset
+                var offset_x = e.clientX - mouse_pos_x;
+                var offset_y = e.clientY - mouse_pos_y;
+
+                //Use relative x,y pos to check if window will still be on screen
+                var bounds_x = initPosX_relative + offset_x;
+                var bounds_y = initPosY_relative + offset_y;
+
+                //Check bounds and move window if the window will not be off screen
+                if ((bounds_x >= 20) &&
+                    (bounds_x + $(id).width()) <= (window_x - 20) &&
+                    (bounds_y >= 20) &&
+                    (bounds_y + $(id).height())<= (window_y - 20)) {
+
+                    //Readjust CSS
+                    $(id).css({"left" : initPosX + offset_x + "px", "top" : initPosY + offset_y + "px"});
+                }
+            });
+        
+    }
