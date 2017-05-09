a8f8047eda8fafb9e9d08fee1b9768b3cc64f40c
nnsookwon <nnsookwon@gmail.com>
2017-05-09 15:25:44 -0700

    Push xoauth2_config file

---
+++ config/xoauth2_config.js
@@ -0,0 +1,7 @@
+// xoauth2 credentials to send reset password email
+module.exports = {
+  	mailUser: "biocomposer@gmail.com",
+ 	clientId: "840594398654-jv1dui5u507l5rt7nrcp56mcvtv298gj.apps.googleusercontent.com",
+	clientSecret: "Q9mZ7c9VDoEWtw4PXIUUC2RE",
+	refreshToken: "1/kTiFdb-Pbdukz7FTWGHVrhiapg0vZsfJ35m5D7gdf00"
+}
